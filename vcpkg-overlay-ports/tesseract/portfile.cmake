eba0c851610d307773767eedda78b225e15cb81e
Kaito Udagawa <umireon@kaito.tokyo>
2025-10-01 12:21:01 +0900

    Update portfile.cmake

--- vcpkg-overlay-ports/tesseract/portfile.cmake
+++ vcpkg-overlay-ports/tesseract/portfile.cmake
@@ -45,15 +45,27 @@ vcpkg_copy_tools(TOOL_NAMES tesseract AUTO_CLEAN)
 vcpkg_fixup_pkgconfig()
 
 if("training-tools" IN_LIST FEATURES)
-    list(APPEND TRAINING_TOOLS
-        ambiguous_words classifier_tester combine_tessdata
-        cntraining dawg2wordlist mftraining shapeclustering
-        wordlist2dawg combine_lang_model lstmeval lstmtraining text2image
-        set_unicharset_properties unicharset_extractor merge_unicharsets
-        )
-    vcpkg_copy_tools(TOOL_NAMES ${TRAINING_TOOLS} AUTO_CLEAN)
+  list(
+    APPEND
+    TRAINING_TOOLS
+    ambiguous_words
+    classifier_tester
+    combine_tessdata
+    cntraining
+    dawg2wordlist
+    mftraining
+    shapeclustering
+    wordlist2dawg
+    combine_lang_model
+    lstmeval
+    lstmtraining
+    text2image
+    set_unicharset_properties
+    unicharset_extractor
+    merge_unicharsets
+  )
+  vcpkg_copy_tools(TOOL_NAMES ${TRAINING_TOOLS} AUTO_CLEAN)
 endif()
-
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
