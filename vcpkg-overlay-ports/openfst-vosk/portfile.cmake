f3d876502e963b039d5d1b2d739780c9dba0fcd7
Kaito Udagawa <umireon@kaito.tokyo>
2025-10-22 14:06:45 +0900

    Add OpenFST port to vcpkg overlay and dependencies (#21)
    * Add OpenFST port to vcpkg overlay and dependencies
    
    Introduces a custom vcpkg overlay port for OpenFST with portfile and manifest. Updates vcpkg.json to include openfst as a dependency.
    
    * Add openfst-vosk port and update vcpkg.json
    
    Introduces a custom vcpkg overlay port for openfst-vosk, including patches to disable tests and fix a constructor issue. Adds portfile and manifest for openfst-vosk, and updates the main vcpkg.json to depend on openfst-vosk instead of openfst.
    
    * Remove OpenFST overlay port from vcpkg
    
    Deleted the OpenFST overlay port files (portfile.cmake and vcpkg.json) from vcpkg-overlay-ports. This cleans up the repository by removing the custom port definition.
    
    * Update vcpkg.json
    
    * Update portfile.cmake

---
+++ vcpkg-overlay-ports/openfst-vosk/portfile.cmake
@@ -0,0 +1,33 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO kkm000/openfst
+    REF win/1.7.2.1
+    SHA512 884d98eca7721dbe297dd376fcbda507e3d242a819ed39f95eebfc3285cb97b10b3aa9969ddcf42af38396311fef9b65bc60ebe50222026e262d28e4ccedde39
+    HEAD_REF master
+    PATCHES
+        0000-disable-tests.patch
+        0001-fix-bitable.patch
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DHAVE_BIN=ON
+        -DHAVE_SCRIPT=OFF
+        -DHAVE_COMPACT=OFF
+        -DHAVE_COMPRESS=OFF
+        -DHAVE_CONST=OFF
+        -DHAVE_FAR=OFF
+        -DHAVE_GRM=OFF
+        -DHAVE_PDT=OFF
+        -DHAVE_MPDT=OFF
+        -DHAVE_LINEAR=OFF
+        -DHAVE_LOOKAHEAD=OFF
+        -DHAVE_NGRAM=OFF
+        -DHAVE_PYTHON=OFF
+        -DHAVE_SPECIAL=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/COPYING)
