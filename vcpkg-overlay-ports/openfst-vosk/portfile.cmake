0cfea2699d91f2c19da51816bc14fb916894ea99
Kaito Udagawa <umireon@kaito.tokyo>
2025-10-22 16:51:04 +0900

    Update portfile.cmake (#24)
    * Update portfile.cmake
    
    * Update portfile.cmake
    
    * Update portfile.cmake

--- vcpkg-overlay-ports/openfst-vosk/portfile.cmake
+++ vcpkg-overlay-ports/openfst-vosk/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kkm000/openfst
-    REF win/1.7.2.1
-    SHA512 884d98eca7721dbe297dd376fcbda507e3d242a819ed39f95eebfc3285cb97b10b3aa9969ddcf42af38396311fef9b65bc60ebe50222026e262d28e4ccedde39
-    HEAD_REF master
+    REF 805841777a283c523ce17ad4efdb3ada776c4862
+    SHA512 399162cbb9f37f4df7c4b0fb6d23e6a5e8737d8a1e92647eb6734c0ec11b7ae749a24a789ce43003a013c9d0a34addbbdb63da2f302a03d34cd3d8d5ab4540f3
+    HEAD_REF winport
     PATCHES
         0000-disable-tests.patch
         0001-fix-bitable.patch
