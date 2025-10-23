7a4566d1eefdb8b00d7afbc3e317cd7d34e0f226
Kaito Udagawa <umireon@kaito.tokyo>
2025-10-23 21:20:48 +0900

    Add vosk api (#36)
    * Add vosk-api port and update vcpkg dependencies
    
    Introduces a new overlay port for vosk-api with corresponding portfile and vcpkg.json. Removes kaldi from the main vcpkg.json dependencies and adds vosk-api instead.
    
    * Add /bigobj flag patch for vosk-api on MSVC
    
    Introduces a patch to add the /bigobj compiler flag when building vosk-api with MSVC to address object file size limitations. Updates portfile.cmake to apply the new patch during the build process.

---
+++ vcpkg-overlay-ports/vosk-api/portfile.cmake
@@ -0,0 +1,21 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO alphacep/vosk-api
+    REF v0.3.50
+    SHA512 cc627fefd87336a7507444ffa201d1ef0b325fdc79659e6dc0f616671d82226977d567decda414b4a1b17986a9755e400c135d9637069dc96a067240a69484fa
+    HEAD_REF master
+    PATCHES
+        0000-add-bigobj.patch
+)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DBUILD_SHARED_LIBS=${BUILD_SHARED}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/COPYING)
