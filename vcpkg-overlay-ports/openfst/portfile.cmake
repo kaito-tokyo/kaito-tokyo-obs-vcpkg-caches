961e8f7e140b35e7f05fa62ab06d537a616832b1
Kaito Udagawa <umireon@kaito.tokyo>
2025-10-24 06:17:18 +0900

    Update OpenFst CMake install paths and config fixup
    Changed CMake install destination for OpenFst config files from 'cmake/openfst' to 'openfst' for consistency. Updated portfile to remove debug include directory and specify PACKAGE_NAME in vcpkg_cmake_config_fixup.

--- vcpkg-overlay-ports/openfst/portfile.cmake
+++ vcpkg-overlay-ports/openfst/portfile.cmake
@@ -19,6 +19,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_cmake_config_fixup(PACKAGE_NAME openfst)
 
 vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/COPYING)
