fa32091ec8a8ab32b6db41f46f2e971c4c1a9aca
Kaito Udagawa <umireon@kaito.tokyo>
2025-10-23 17:45:01 +0900

    Update portfile.cmake

--- vcpkg-overlay-ports/kaldi/portfile.cmake
+++ vcpkg-overlay-ports/kaldi/portfile.cmake
@@ -21,4 +21,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
+file(MAKE_DIRECTORY
+    ${CURRENT_PACKAGES_DIR}/share/unofficial-openfst
+    ${CURRENT_PACKAGES_DIR}/debug/share/unofficial-openfst
+)
+vcpkg_cmake_config_fixup()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
