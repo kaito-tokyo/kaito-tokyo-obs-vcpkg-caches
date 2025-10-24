930bd4f4b891d4f080231d01dc49d864254d8a73
Kaito Udagawa <umireon@kaito.tokyo>
2025-10-24 09:09:55 +0900

    Update vosk-api patch and portfile for improved vcpkg support
    Replaces 0000-fix-libname.patch with a new 0000-fix-for-vcpkg.patch that enhances CMake integration, target exports, and config file generation for vosk-api. Updates portfile.cmake to use the new patch and adds steps for cleaning up debug includes and fixing up CMake config. These changes improve downstream usage and packaging consistency.

--- vcpkg-overlay-ports/vosk-api/portfile.cmake
+++ vcpkg-overlay-ports/vosk-api/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 cc627fefd87336a7507444ffa201d1ef0b325fdc79659e6dc0f616671d82226977d567decda414b4a1b17986a9755e400c135d9637069dc96a067240a69484fa
     HEAD_REF master
     PATCHES
-        0000-fix-libname.patch
+        0000-fix-for-vcpkg.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
@@ -16,6 +16,9 @@ vcpkg_cmake_configure(
         -DBUILD_SHARED_LIBS=${BUILD_SHARED}
 )
 
+
 vcpkg_cmake_install()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_cmake_config_fixup(PACKAGE_NAME vosk)
 
 vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/COPYING)
